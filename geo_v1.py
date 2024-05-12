c0eb875e3c3218d1cee0be96b2cf8b76ba7dad41
KubKro <166413249+KubKro@users.noreply.github.com>
2024-05-12 20:47:39 +0200

    skończony kod cz2
    poprawiono komende plh2xyz
    dodano komende BL2PL2000
    dodano header_lines
    dodano wiadomości błędów wyskakujących w przypadkach nie stosowania się do pliku README.txt

--- geo_v1.py
+++ geo_v1.py
@@ -1,5 +1,6 @@
-from math import sin, cos, sqrt, atan, atan2, degrees, radians
+from math import sin, cos, sqrt, tan, atan, atan2, degrees, radians, pi 
 import sys
+import numpy as np
 
 o = object()
 
@@ -76,27 +77,165 @@ class Transformacje:
             
 
     def plh2xyz(self, phi, lam, h):
+        """
+        Transformajca odwrotna do transformacji Hirvonena. Funkcja przyjmuje współrzędne geodezyjne długość szerokość i wysokośc elipsoidalna (phi, lam, h) i zwraca 
+        wyniki we współrzędnych ortokartezjańskich (x, y, z)
+        Parameters
+        ----------
+        lat
+            [stopnie dziesiętne] - szerokość geodezyjna
+        lon
+            [stopnie dziesiętne] - długośc geodezyjna.
+        h : TYPE
+            [metry] - wysokość elipsoidalna
+
+        Returns
+        -------
+        X, Y, Z : FLOAT
+             współrzędne w układzie orto-kartezjańskim,
+        """
         
-        N = self.a / sqrt((1 - self.ecc2 * sin(phi)**2))
-        
-        X = (N + h) * cos(phi) * cos(lam)
-        Y = (N + h) * cos(phi) * sin(lam)
-        Z = ((N * (1 - self.ecc2)) + h) * sin(phi)
+        Rn = self.a / sqrt(1 - self.ecc2 * sin(phi)**2)
+        q = Rn * self.ecc2 * sin(phi)
+        X = (Rn + h) * cos(phi) * cos(lam)
+        Y = (Rn + h) * cos(phi) * sin(lam)
+        Z = (Rn + h) * sin(phi) - q
         
         return X, Y, Z
+    
+def pl2000(self, lat, lon):
+        '''
+        Funkcja zwracająca współrzędne w układzie 2000
+    
+        Parameters
+        ----------
+        lat
+            [stopnie dziesiętne] - szerokość geodezyjna
+        lon
+            [stopnie dziesiętne] - długośc geodezyjna.
+   
+    
+        Returns
+        -------
+        X92, Y92 współrzędne w układzie 1992
+    
+        '''
+        phi = lat*pi/180
+        lam = lon*pi/180
         
+        if lam >np.deg2rad(13.5) and lam < np.deg2rad(16.5):
+                strefa = 5
+                lam0 = np.deg2rad(15)
+        elif lam >np.deg2rad(16.5) and lam < np.deg2rad(19.5):
+                strefa = 6
+                lam0 = np.deg2rad(18)
+        elif lam >np.deg2rad(19.5) and lam < np.deg2rad(22.5):
+                strefa =7
+                lam0 = np.deg2rad(21)
+        elif lam >np.deg2rad(22.5) and lam < np.deg2rad(25.5):
+                strefa = 8
+                lam0 = np.deg2rad(24)  
+        
+        A0 = 1 - self.ecc2/4 - (3 * (self.ecc2**2))/64 - (5 * (self.ecc2**3))/256
+        A2 = 3/8 *(self.ecc2 + (self.ecc2**2)/4 + 15 * (self.ecc2**3)/ 128)
+        A4 = 15/256 * (self.ecc2**2 + 3 * (self.ecc2**3)/4)
+        A6 = 35 * (self.ecc2**3)/ 3072
+        sigma = self.a * (A0 * phi - A2 * sin(2*phi) + A4 * sin(4*phi) - A6 * sin(6*phi))
+        
+        b2 = self.a**2 * (1 - self.ecc2)
+        e2prim = (self.a**2 - b2)/b2
+        dl = lam - lam0
+        t = tan(phi)
+        n2 = e2prim * (cos(phi))**2
+        N = self.a/sqrt(1 - (self.ecc2 * sin(phi)**2))
+        xgk = sigma + (dl**2/2) * N * sin(phi) * cos(phi) * (1 + ((dl**2/12) * cos(phi)**2 * (5 - t**2 + 9*n2 + 4*(n2**2))) + ((dl**4/360) * cos(phi)**4 * (61 - 58 * t**2 + t**4 + 270 * n2 - 330 * n2 * t**2)))
+        ygk = dl * N * cos(phi) * (1 + (dl**2/6) * cos(phi)**2 * (1 - t**2 + n2) + (dl**4/120) * cos(phi)**4 * (5 - 18 * t**2 + t**4 + 14*n2 - 58 * n2 * t**2))
+        m2000 = 0.999923
+        X2000 = xgk * m2000 
+        Y2000 = ygk * m2000 + 500000 + strefa * 1000000
+        return X2000, Y2000
 #####definicje transformacji z pliku GW - trzeba będzie je zmienić pod selfy by wpisywały się w model
 
-
-
 if __name__ == "__main__":
-    # utworzenie obiektu
-    geo = Transformacje(model = "wgs84")
-    # dane XYZ geocentryczne
-    X = 3664940.500; Y = 1409153.590; Z = 5009571.170
-    phi, lam, h = geo.xyz2plh(X, Y, Z)
-    print(phi, lam, h)
-    # phi, lam, h = geo.xyz2plh2(X, Y, Z)
-    # print(phi, lam, h)
+    # Checking for command-line arguments
+    if len(sys.argv) <6:
+        print(" <input_file> <header_lines> <ellipsoid_model> <transformation_function>")
+        sys.exit(1)
+
+    input_file = sys.argv[1] 
+    if 'header_lines' in sys.argv:
+        header_lines = int(sys.argv[3])
+        modele_w_obsludze = ["wgs84", "grs80", "mars"]
+        if sys.argv[4] not in modele_w_obsludze:
+             print("Model nie jest obsługiwany.") 
+        else:
+            ellipsoid_model = sys.argv[4]
+            transformation_function = sys.argv[5]
+
+
+    if 'xyz2plh' in sys.argv and 'plh2xyz' in sys.argv:
+        print('możesz podać tylko jedną flagę')
         
-    
+    # Creating Transformacje object
+    geo = Transformacje(model=ellipsoid_model)
+
+    # Performing transformation based on the chosen function
+    if transformation_function == 'xyz2plh':
+        coords_plh = []
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+            lines = lines[header_lines:]
+            for line in lines:
+                line = line.strip()
+                x_str, y_str, z_str = line.split(',')
+                x, y, z = (float(x_str), float(y_str), float(z_str))
+                phi, lam, h = geo.xyz2plh(x, y, z)
+                coords_plh.append([phi, lam, h])
+
+        with open('result_xyz2plh.txt', 'w') as f:
+            f.write('phi[stopnie], lam[stopnie], h[m]\n')
+            for coords in coords_plh:
+                line = ','.join([f'{coord:11.3f}' for coord in coords])
+                f.write(line + '\n')
+
+    if transformation_function == 'plh2xyz':
+        coords_xyz = []
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+            lines = lines[header_lines:]
+            for line in lines:
+                line = line.strip()
+                phi_str, lam_str, h_str = line.split(',')
+                phi, lam, h = (float(phi_str), float(lam_str), float(h_str))
+                x, y, z = geo.plh2xyz(phi, lam, h)
+                coords_xyz.append([x, y, z])
+
+        with open('result_plh2xyz.txt', 'w') as f:
+            f.write('X[m], Y[m], Z[m]\n')
+            for coords in coords_xyz:
+                line = ','.join([f'{coord:11.3f}' for coord in coords])
+                f.write(line + '\n')
+
+    elif transformation_function == 'pl2000':
+        coords_pl2000 = []
+        with open(input_file, 'r') as f:
+            lines = f.readlines()
+            lines = lines[header_lines:]
+            for line in lines:
+                line = line.strip()
+                phi_str, lam_str, h_str = line.split(',')
+                phi, lam, h = (float(phi_str), float(lam_str), float(h_str))
+                f2000, l2000 = geo.pl2000(phi, lam)
+                if lam < np.deg2rad(13.5) and lam > np.deg2rad(25.5):
+                    print("Punkt poza strefami odwzorowawczymi układu PL-2000")  
+                coords_pl2000.append([f2000, l2000])
+
+        with open('result_pl1992z.txt', 'w') as f:
+            f.write('x92, y92\n')
+            for coords in coords_pl2000:
+                line = ','.join([f'{coord:11.3f}' for coord in coords])
+                f.write(line + '\n')
+
+    else:
+        print("Transformation function not recognized.")
+        sys.exit(1)
